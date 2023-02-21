37dbc8e3f41e3e46c8cb97d9a054d9f9c6870db0
shobannah <116910257+shobannah@users.noreply.github.com>
2023-02-21 09:13:23 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # NoteTaker
+
+## Description
+
+This is a Note Taker that can be used to write and save notes.
+
+## Screenshot
+
+![image](./assets/images/Screenshot.png)
+
+## Link
+
+<a href=https://github.com/shobannah/NoteTaker.git> Note Taker
--- server.js
+++ server.js
@@ -19,7 +19,15 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
   });
   
-app.get('/api/notes', (req, res) => {res.json(notesData)});
+app.get('/api/notes', (req, res) => {res.json(notesData)
+  const { title , text } = req.body;
+  if (title && text ) {
+    const newNote = {
+      title,
+      text,
+      review_id: uuid(),
+    };
+});
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
@@ -30,32 +38,26 @@ app.get('/*', (req, res) => {
 
 app.post('/api/notes', (req, res) => {
   
-  // Destructuring assignment for the items in req.body
   const { title , text } = req.body;
 
-  // If all the required properties are present
+
   if (title && text ) {
-    // Variable for the object we will save
     const newNote = {
       title,
       text,
       review_id: uuid(),
     };
 
-    // Obtain existing reviews
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
       if (err) {
         console.error(err);
       
       
       } else {
-        // Convert string into JSON object
         const parsedNotes = JSON.parse(data);
 
-        // Add a new review
         parsedNotes.push(newNote);
 
-        // Write updated reviews back to the file
         fs.writeFile(
           './db/db.json',
           JSON.stringify(parsedNotes, null, 3),
