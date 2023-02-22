f7f42b714684c5c174b44cffaccfcd9726324a9e
shobannah <116910257+shobannah@users.noreply.github.com>
2023-02-21 23:37:20 -0500

    updated get request

--- server.js
+++ server.js
@@ -1,83 +1,76 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
-const notesData = require('./db/db.json');
-const uuid = require('./helpers/uuid');
+const notesData = require("./db/db.json");
+const uuid = require("./helpers/uuid");
 const PORT = process.env.PORT || 5500;
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedNotes = JSON.parse(data);
 
-
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+      res.json(parsedNotes);
+    }
   });
-  
-app.get('/api/notes', (req, res) => res.json(notesData));
+});
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-    });
-      
+app.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
 
-
-
-app.post('/api/notes', (req, res) => {
-  
-  const { title , text } = req.body;
-
-
-  if (title && text ) {
+  if (title && text) {
     const newNote = {
       title,
       text,
       id: uuid(),
-
-
     };
 
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
       if (err) {
         console.error(err);
-      
-      
       } else {
         const parsedNotes = JSON.parse(data);
 
         parsedNotes.push(newNote);
 
         fs.writeFile(
-          './db/db.json',
+          "./db/db.json",
           JSON.stringify(parsedNotes, null, 3),
           (writeErr) =>
             writeErr
               ? console.error(writeErr)
-              : console.info('Successfully updated notes!')
-        )
+              : console.info("Successfully updated notes!")
+        );
       }
     });
 
     const response = {
-      status: 'success',
+      status: "success",
       body: newNote,
     };
 
-    console.log(response) ;
+    console.log(response);
     res.status(201).json(response);
   } else {
-    res.status(500).json('Error in posting note');
+    res.status(500).json("Error in posting note");
   }
-
-
 });
 
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
-  })
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
